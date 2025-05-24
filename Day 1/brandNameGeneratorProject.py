f863214a8115181e8304f09780e9493e2eec4ec2
Adib <adib1690@gmail.com>
2025-05-24 23:36:48 +0600

    Add Day 1 Python practice code

---
+++ Day 1/brandNameGeneratorProject.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Game Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+print(city_name)
+pet_name = input("What's your pet's name?\n")
+print(pet_name)
+print("Your band name could be " + city_name + " " + pet_name)
