6e3e2339fb05a6de367d33742455e7191d4b5e94
Adib <adib1690@gmail.com>
2025-06-01 00:38:25 +0600

    Add hurdle4-v1 and hurdle4-v2 solutions to Day 6

---
+++ Day 6/hurdle4-v2.py
@@ -0,0 +1,24 @@
+# This one is minimalistic and works fine.
+# Hurdle 4 - Version 2
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()  
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
